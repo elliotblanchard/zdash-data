c85e6df687058b98c276f889fa28dd0fc77904d7
Elliot Blanchard <git@elliotblanchard.com>
2020-11-24 18:34:48 -0500

    added base of multithreading

--- app/main.rb
+++ app/main.rb
@@ -5,11 +5,57 @@ require 'pry'
 require 'colorize'
 require_relative './models/transaction'
 
-max_block_size = 20
-
 def db_configuration
   db_configuration_file = File.join(File.expand_path('..', __FILE__), '..', 'db', 'config.yml')
   YAML.load(File.read(db_configuration_file))
+end
+
+def get_transactions_block(offset = 0)
+  max_block_size = 20
+
+  request_uri = "https://api.zcha.in/v2/mainnet/transactions?sort=timestamp&direction=descending&limit=#{max_block_size}&offset=#{offset}"
+  buffer = open(request_uri).read
+  transactions = JSON.parse(buffer)
+
+  transactions.each_with_index do |transaction, index|
+    t = Transaction.create(
+      zhash: transaction['hash'],
+      mainChain: transaction['mainChain'],
+      fee: transaction['fee'],
+      ttype: transaction['type'],
+      shielded: transaction['shielded'],
+      index: transaction['index'],
+      blockHash: transaction['blockHash'],
+      blockHeight: transaction['blockHeight'],
+      version: transaction['version'],
+      lockTime: transaction['lockTime'],
+      timestamp: transaction['timestamp'],
+      time: transaction['time'],
+      vin: transaction['vin'],
+      vout: transaction['vout'],
+      vjoinsplit: transaction['vjoinsplit'],
+      vShieldedOutput: transaction['vShieldedOutput'],
+      vShieldedSpend: transaction['vShieldedSpend'],
+      valueBalance: transaction['valueBalance'],
+      value: transaction['value'],
+      outputValue: transaction['outputValue'],
+      shieldedValue: transaction['shieldedValue'],
+      overwintered: transaction['overwintered']
+    )
+
+    transaction_time = Time.at(transaction['timestamp']).to_datetime.strftime('%I:%M%p %a %m/%d/%y')
+
+    print "#{index+1}. Transaction "
+    print "#{transaction['hash'][0..10]}... ".colorize(:light_blue)
+    print "at "
+    print "#{transaction_time} ".colorize(:yellow)
+ 
+    if t.valid?
+      print "saved \n".colorize(:green)
+    else
+      print "not saved #{t.errors.messages} \n".colorize(:red)
+    end
+  end
 end
 
 ActiveRecord::Base.establish_connection(db_configuration['development'])
@@ -20,46 +66,5 @@ ActiveRecord::Base.establish_connection(db_configuration['development'])
 # last entry in the DB before you start your pass or something like that.
 # Probably better to go through the entire DAY the last transaction in the DB was on so you minimize missed entries
 
-request_uri = "https://api.zcha.in/v2/mainnet/transactions?sort=timestamp&direction=descending&limit=#{max_block_size}&offset=0"
-buffer = open(request_uri).read
-transactions = JSON.parse(buffer)
-
-transactions.each_with_index do |transaction, index|
-  t = Transaction.create(
-    zhash: transaction['hash'], 
-    mainChain: transaction['mainChain'],
-    fee: transaction['fee'], 
-    ttype: transaction['type'],
-    shielded: transaction['shielded'],
-    index: transaction['index'],
-    blockHash: transaction['blockHash'],
-    blockHeight: transaction['blockHeight'],
-    version: transaction['version'],
-    lockTime: transaction['lockTime'],
-    timestamp: transaction['timestamp'],
-    time: transaction['time'],
-    vin: transaction['vin'],
-    vout: transaction['vout'],
-    vjoinsplit: transaction['vjoinsplit'],
-    vShieldedOutput: transaction['vShieldedOutput'],
-    vShieldedSpend: transaction['vShieldedSpend'],
-    valueBalance: transaction['valueBalance'],
-    value: transaction['value'],
-    outputValue: transaction['outputValue'],
-    shieldedValue: transaction['shieldedValue'],
-    overwintered: transaction['overwintered']
-  )
-
-  transaction_time = Time.at(transaction['timestamp']).to_datetime.strftime('%I:%M%p %a %m/%d/%y')
-
-  print "#{index+1}. Transaction "
-  print "#{transaction['hash'][0..10]}... ".colorize(:light_blue)
-  print "at "
-  print "#{transaction_time} ".colorize(:yellow)
-
-  if t.valid?
-    print "saved \n".colorize(:green)
-  else
-    print "not saved #{t.errors.messages} \n".colorize(:red)
-  end
-end
+t1 = Thread.new{ get_transactions_block(0) }
+t1.join # waits for the thread to finish before ending the program (otherwise program ends before anything is recieved / printed)

b07e02407a9f5970455de627492be5617f162ef8
Elliot Blanchard <git@elliotblanchard.com>
2020-11-26 15:50:49 -0500

    Replaced a couple of hardcoded values

--- app/main.rb
+++ app/main.rb
@@ -12,6 +12,7 @@ end
 
 def get_transactions_block(offset = 0,last_timestamp)
   max_block_size = 20
+  transaction_overlap = 3600
   uri_base = 'https://api.zcha.in/v2/mainnet/transactions?sort=timestamp&direction=descending&limit='
 
   request_uri = "#{uri_base}#{max_block_size}&offset=#{offset}"
@@ -24,7 +25,7 @@ def get_transactions_block(offset = 0,last_timestamp)
     print "\nError #{e.inspect}, retrying (attempt #{retries})...".colorize(:cyan)
     sleep(30)
     $server_error = true
-    print "retrying"
+    print 'retrying'
     retry
   end
 
@@ -60,10 +61,10 @@ def get_transactions_block(offset = 0,last_timestamp)
 
     print "\n#{offset+index+1}. "
     print "#{transaction['hash'][0..10]}... ".colorize(:light_blue)
-    print "at "
+    print 'at '
     print "#{transaction_time} ".colorize(:yellow)
-    print "/ "
-    print "#{transaction['timestamp'].to_i - (last_timestamp - 3600)} ".colorize(:blue)
+    print '/ '
+    print "#{transaction['timestamp'].to_i - (last_timestamp - transaction_overlap)} ".colorize(:blue)
  
     if t.valid?
       print "saved".colorize(:green)
@@ -71,57 +72,51 @@ def get_transactions_block(offset = 0,last_timestamp)
       print "not saved #{t.errors.messages}".colorize(:red)
     end
 
-    # If timestamp of last transaction was last_timestamp - 3600 (seconds = 1 hour), 
-    # set job_complete to TRUE so loop ends
-    if transaction['timestamp'].to_i < (last_timestamp - 3600)
+    # If timestamp of last transaction was last_timestamp - transaction_overlap
+    # set job_complete to TRUE so loop ends. The overlap is to catch any transactions
+    # that may have been missed in the last loop
+    if transaction['timestamp'].to_i < (last_timestamp - transaction_overlap)
       $job_complete = true
       break
     end
   end
 end
 
-ActiveRecord::Base.establish_connection(db_configuration['development'])
-
-last_timestamp = Transaction.maximum('timestamp')
-
 $job_complete = false
 $server_error = false
-
 offset = 0
-rest_counter = 0
+offset_increment = 15
+interval_thread_launch = 0.2
+interval_server_down = 120
+interval_threads_complete = 60
+interval_jobs = 18000
+
+ActiveRecord::Base.establish_connection(db_configuration['development'])
+last_timestamp = Transaction.maximum('timestamp')
 
 # Parent loop to get new transactions every few hours
 while 1 == 1
-    # Main loop to get latest transactions (while job complete == false)
-    while $job_complete == false
-        # Launch a new thread - if the server is active
-        if $server_error == false
-            Thread.new{ get_transactions_block(offset, last_timestamp) }
-
-            # WAIT 1/5 second
-            sleep(1)
-
-            # Increase offset by 15
-            offset += 15
-            rest_counter += 1
-
-            if rest_counter == 250
-            print "\nPausing execution"
-            sleep(60)
-            rest_counter = 0
-            end
-        else
-            print "\nServer is down. Pausing for 120 seconds"
-            sleep(120)
-            print "\nResuming"
-            $server_error = false
-        end
+  # Main loop to get latest transactions (while job complete == false)
+  while $job_complete == false
+    # Launch a new thread - if the server is active
+    if $server_error == false
+      Thread.new{ get_transactions_block(offset, last_timestamp) }
+      sleep(interval_thread_launch)
+      offset += offset_increment
+    else
+      print "\nServer is down. Pausing for #{interval_server_down} seconds"
+      sleep(interval_server_down)
+      print "\nResuming"
+      $server_error = false
     end
-    # WAIT 60 seconds for all threads to complete
-    sleep(60)
-    print "\nFinished getting latest transactions. Current time is: #{DateTime.now.strftime('%I:%M%p %a %m/%d/%y')}. Waiting for 5 hours..."
-    sleep(5.hours)
+  end
+  # WAIT for all threads to complete
+  sleep(interval_threads_complete)
+  print "\nFinished getting latest transactions."
+  print " Current time is: #{DateTime.now.strftime('%I:%M%p %a %m/%d/%y')}."
+  print " Waiting for #{interval_jobs/3600} hours..."
+  sleep(interval_jobs)
 end
 
-# WAIT 60 seconds for all threads to complete
-sleep(60)
+# WAIT for all threads to complete
+sleep(interval_threads_complete)

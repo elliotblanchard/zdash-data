025f963cd87aaa1bdb142a9112b3caecb0ac65e1
Elliot Blanchard <git@elliotblanchard.com>
2020-11-27 13:44:54 -0500

    Increased interval between thread launches - the interval was too short, causing the app to request too many transactions before the first one arrived

--- app/main.rb
+++ app/main.rb
@@ -11,6 +11,11 @@ def db_configuration
 end
 
 def get_transactions_block(offset = 0,last_timestamp)
+
+  print "\nLaunching thread."
+  print "\nOffeset is: #{offset}"
+  print " Current time is: #{DateTime.now.strftime('%I:%M%p %a %m/%d/%y')}."
+
   max_block_size = 20
   transaction_overlap = 3600
   uri_base = 'https://api.zcha.in/v2/mainnet/transactions?sort=timestamp&direction=descending&limit='
@@ -63,6 +68,7 @@ def get_transactions_block(offset = 0,last_timestamp)
     print "#{transaction['hash'][0..10]}... ".colorize(:light_blue)
     print 'at '
     print "#{transaction_time} ".colorize(:yellow)
+    print "timestamp: #{transaction['timestamp']} "
     print '/ '
     print "#{transaction['timestamp'].to_i - (last_timestamp - transaction_overlap)} ".colorize(:blue)
  
@@ -76,15 +82,16 @@ def get_transactions_block(offset = 0,last_timestamp)
     # set job_complete to TRUE so loop ends. The overlap is to catch any transactions
     # that may have been missed in the last loop
     if transaction['timestamp'].to_i < (last_timestamp - transaction_overlap)
-      $job_complete = true
-      print "\nJob complete!"
+      $overlaps += 1
+      print "\nOverlaps found: #{$overlaps}"
       break
     end
   end
 end
 
+overlaps_to_stop = 200
 offset_increment = 15
-interval_thread_launch = 0.2
+interval_thread_launch = 2
 interval_server_down = 120
 interval_threads_complete = 60
 interval_jobs = 18000
@@ -93,13 +100,14 @@ ActiveRecord::Base.establish_connection(db_configuration['development'])
 
 # Parent loop to get new transactions every few hours
 while 1 == 1
-  $job_complete = false
+  # $job_complete = false
+  $overlaps = 0
   $server_error = false
   offset = 0
   last_timestamp = Transaction.maximum('timestamp')
   print "\nGetting new transactions. Last timestamp is: #{last_timestamp}"
   # Main loop to get latest transactions (while job complete == false)
-  while $job_complete == false
+  while $overlaps < overlaps_to_stop
     # Launch a new thread - if the server is active
     if $server_error == false
       Thread.new{ get_transactions_block(offset, last_timestamp) }

cd43d4f423ff7ca1c5c744823ad1dfccd08deb94
Elliot Blanchard <git@elliotblanchard.com>
2021-01-04 16:59:40 -0500

    Continuing to update

--- app/main.rb
+++ app/main.rb
@@ -11,8 +11,10 @@ def db_configuration
   YAML.load(File.read(db_configuration_file))
 end
 
-def get_transactions_block(offset = 0,last_timestamp, log_file, trans_saved, trans_failed)
+def get_transactions_block(offset = 0,last_timestamp, log_file)
 
+  trans_saved = 0
+  trans_failed = 0
   max_block_size = 20
   retry_pause = 30
   max_retries = 20
@@ -67,22 +69,20 @@ def get_transactions_block(offset = 0,last_timestamp, log_file, trans_saved, tra
 
     category = Classify.classify_transaction(t)
     unless t.update(category: category)
-      t.destroy # Because duplicate zhash 2:04
+      t.destroy # Because duplicate zhash
     end
-
+ 
     transaction_time = Time.at(transaction['timestamp']).to_datetime.strftime('%I:%M%p %a %m/%d/%y')
 
-=begin
     print "\n#{offset+index+1}. "
     print "#{transaction['hash'][0..5]}... ".colorize(:light_blue)
     print 'category '
-    print "#{transaction['category']}. ".colorize(:light_blue)
+    print "#{t['category']}. ".colorize(:light_blue)
     print 'at '
     print "#{transaction_time} ".colorize(:yellow)
     print "timestamp: #{transaction['timestamp']} "
     print '/ '
     print "#{transaction['timestamp'] - last_timestamp} ".colorize(:blue)
-=end
  
     if t.valid?
       # print "saved".colorize(:green) # keep count of number saved for log
@@ -92,7 +92,7 @@ def get_transactions_block(offset = 0,last_timestamp, log_file, trans_saved, tra
       trans_failed += 1
     end
   end
-  transactions.last['timestamp']
+  return_arr = [transactions.last['timestamp'], trans_saved, trans_failed]
 end
 
 offset_increment = 15
@@ -109,22 +109,26 @@ else
 end
 
 offset = 0
-overlap = 450 # 7.5 minutes
+overlap = 900 # 15 minutes
 trans_saved = 0
 trans_failed = 0
 last_timestamp = Transaction.maximum('timestamp')
 current_timestamp = Float::INFINITY
 
 log_file.write("Getting new transactions. Last timestamp is: #{last_timestamp}\n")
+print("Getting new transactions. Last timestamp is: #{last_timestamp}\n")
 
-=begin
 while (last_timestamp - overlap) < current_timestamp
-  current_timestamp = get_transactions_block(offset, last_timestamp, log_file, trans_saved, trans_failed)
+  return_data = get_transactions_block(offset, last_timestamp, log_file)
+  current_timestamp = return_data[0]
+  trans_saved += return_data[1]
+  trans_failed += return_data[2]
   offset += offset_increment
 end
-=end
+
 log_file.write("Finished getting latest transactions. #{trans_saved} saved and #{trans_failed} failed.\n")
 log_file.write("Current time is: #{DateTime.now.strftime('%I:%M%p %a %m/%d/%y')}.\n\n")
+print("Current time is: #{DateTime.now.strftime('%I:%M%p %a %m/%d/%y')}.\n\n")
 exit(true)
 # print " Waiting for #{interval_jobs/3600.0} hours..."
 

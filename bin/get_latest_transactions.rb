5b384d171d1fda6fcf3a29380467d073d3605d2a
Elliot Blanchard <git@elliotblanchard.com>
2021-01-10 13:45:46 -0500

    Added db to script

--- bin/get_latest_transactions.rb
+++ bin/get_latest_transactions.rb
@@ -1,2 +1,18 @@
 #!/usr/bin/env ruby
-print "Testing"
+require 'active_record'
+require 'activerecord-import'
+require 'json'
+require 'open-uri'
+require 'pry'
+require 'colorize'
+require_relative '../app/models/transaction'
+require_relative '../app/helpers/classify'
+
+def db_configuration
+  db_configuration_file = File.join(File.expand_path('..', __FILE__), '..', 'db', 'config.yml')
+  YAML.load(File.read(db_configuration_file))
+end
+
+ActiveRecord::Base.establish_connection(db_configuration['development'])
+
+puts Transaction.maximum('timestamp')
